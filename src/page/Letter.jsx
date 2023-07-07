853ed7e72c47795f3d56528e2346bf6dd9bc59f1
stella0905 <o122233@naver.com>
2023-07-08 02:59:13 +0900

    Design: letter 페이지 UI완료

--- src/page/Letter.jsx
+++ src/page/Letter.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
+import letterImg from '../assets/letterimg.png'
+import useInput from '../components/useInput'
 
 const Letter = () => {
+  const [letter,onChangeLetter] = useInput('');
+
   return (
-    <LetterDiv>Letter</LetterDiv>
+    <LetterDiv>
+      <EditorTextarea value={letter} onChange={onChangeLetter}/>
+    </LetterDiv>
   )
 }
 
 export default Letter
 
 const LetterDiv = styled.div`
-  border:1px solid red;
+  background-image: url(${letterImg});
+  background-size: 640px 830px ;
   height:830px;
+`
+const EditorTextarea = styled.textarea`
+  margin-top:60px;
+  margin-left:50px;
+  width:505px;
+  height:470px;
+  padding:15px;
+  border:none;
+
+  font-size:28px;
+  &:focus {
+    outline: none;
+  }
 `
